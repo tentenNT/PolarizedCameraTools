60056f9ae1f9a52da05c58677bee5ec9df184e1e
tentenNT <takuminpc@gmail.com>
2019-09-13 19:27:58 -1000

    偏光情報をクラス化

--- tool_kit.cpp
+++ tool_kit.cpp
@@ -11,87 +11,104 @@
 
 #define WIDTH                                   2448
 #define HEIGHT                                  2048
-#define FILE_NAME                               "polarize.raw"
-#define FILE_NAME_0DEG                  "0degree.raw"
-#define FILE_NAME_45DEG                 "45degree.raw"
-#define FILE_NAME_90DEG                 "90degree.raw"
-#define FILE_NAME_135DEG                "135degree.raw"
-#define FILE_NAME_0DEG_BGR              "0degree_bgr.raw"
-#define FILE_NAME_45DEG_BGR             "45degree_bgr.raw"
-#define FILE_NAME_90DEG_BGR             "90degree_bgr.raw"
-#define FILE_NAME_135DEG_BGR    "135degree_bgr.raw"
-#define RED_GAIN                                1.35
-#define GREEN_GAIN                              1.0
-#define BLUE_GAIN                               2.87
 #define TIMEOUT                                 2000
 #define numberOfSeconds                         10
 #define waitTime                                1
+
 using namespace Arena;
 
-void GetPolarizeData(IImage* pImage, void* pDeg0, void* pDeg45, void* pDeg90, void* pDeg135){
-    const uint32_t pixelFormat = static_cast<uint32_t>(pImage->GetPixelFormat());
-    uint8_t* inputBuff = const_cast<uint8_t*>(pImage->GetData());
-    uint8_t* src = inputBuff;
-    unsigned char* dest0 = (unsigned char*)pDeg0;
-    unsigned char* dest45 = (unsigned char*)pDeg45;
-    unsigned char* dest90 = (unsigned char*)pDeg90;
-    unsigned char* dest135 = (unsigned char*)pDeg135;
+//アクセス制限をかけたいけど現状では全てpublic
+struct Polarized{
+    //データメンバの宣言
+    void *pDeg0, *pDeg45, *pDeg90, *pDeg135;
+    float i_max, i_min, rho, theta;
+    cv::Mat deg0_mat, deg45_mat, deg90_mat, deg135_mat;
 
-    // 偏光データを取り出しメモリに格納
-    for (int y = 0; y < HEIGHT; y += 2){
-        uint8_t* srcImage = src + y * WIDTH;
-        for (int x = 0; x < WIDTH; x += 2){
-            *dest90++ = *srcImage;
-            *dest45++ = *(srcImage + 1);
-            *dest135++ = *(srcImage + WIDTH);
-            *dest0++ = *(srcImage + WIDTH + 1);
-            srcImage += 2;
+    //コンストラクタ
+    Polarized(void* pDeg0, void* pDeg45, void* pDeg90, void* pDeg135)
+        : pDeg0(pDeg0), pDeg45(pDeg45), pDeg90(pDeg90), pDeg135(pDeg135){}
+
+    void GetPolarizedData(IImage* pImage){
+        uint8_t* inputBuff = const_cast<uint8_t*>(pImage->GetData());
+        uint8_t* src = inputBuff;
+        // このようにする事でpDegは変化せずdestのポインタの指す位置が変化し続ける
+        unsigned char* dest0 = (unsigned char*)pDeg0;
+        unsigned char* dest45 = (unsigned char*)pDeg45;
+        unsigned char* dest90 = (unsigned char*)pDeg90;
+        unsigned char* dest135 = (unsigned char*)pDeg135;
+
+        // 偏光データを取り出しメモリに格納
+        for (int y = 0; y < HEIGHT; y += 2){
+            uint8_t* srcImage = src + y * WIDTH;
+            for (int x = 0; x < WIDTH; x += 2){
+                // *i++の場合，*iの処理が終わった後，ポインタのインクリメントが行われる
+                // *(i++)も同様
+                *dest90++ = *srcImage;
+                *dest45++ = *(srcImage + 1);
+                *dest135++ = *(srcImage + WIDTH);
+                *dest0++ = *(srcImage + WIDTH + 1);
+                srcImage += 2;
+            }
         }
     }
-}
 
-//I_max, I_minを計算
-void CalculateIntensity(void* pDeg0, void* pDeg45, void* pDeg90, void* pDeg135){
-}
+    // ポインタからcv::Matへ変換
+    void TranslatePointerToMatrix(IImage* pImage){
+        deg0_mat = cv::Mat((int)pImage->GetHeight() / 2, (int)pImage->GetWidth() / 2, CV_8UC1, pDeg0);
+        deg45_mat = cv::Mat((int)pImage->GetHeight() / 2, (int)pImage->GetWidth() / 2, CV_8UC1, pDeg45);
+        deg90_mat = cv::Mat((int)pImage->GetHeight() / 2, (int)pImage->GetWidth() / 2, CV_8UC1, pDeg90);
+        deg135_mat = cv::Mat((int)pImage->GetHeight() / 2, (int)pImage->GetWidth() / 2, CV_8UC1, pDeg135);
+    };
 
-//OpenCVで動画像を取得
-void RunOpencvVideo(IDevice* pDevice, GenApi::INodeMap* pNodeMap){
-    gcstring windowName = GetNodeValue<GenICam::gcstring>(pNodeMap, "DeviceModelName") + " (" + GetNodeValue<GenICam::gcstring>(pNodeMap, "DeviceSerialNumber") + ")";
+    // I_max, I_minを計算
+    void CalculateIntensity(){
+    };
 
-    IImage *pImage;
-    cv::Mat img;
+};
+    
 
-    double framesPerSecond = GetNodeValue<double>(pNodeMap, "AcquisitionFrameRate");
 
-    for (int i = 0; i < framesPerSecond * numberOfSeconds;  i++){
-        pImage = pDevice->GetImage(TIMEOUT);
-        img = cv::Mat((int)pImage->GetHeight(), (int)pImage->GetWidth(), CV_8UC1, (void *)pImage->GetData());
-        //イメージのリサイズ
-        cv::resize(img, img, cv::Size(), 0.5, 0.5);
+//// OpenCVで動画像を取得
+//void RunOpencvVideo(IDevice* pDevice, GenApi::INodeMap* pNodeMap){
+//    gcstring windowName = GetNodeValue<GenICam::gcstring>(pNodeMap, "DeviceModelName") + " (" + GetNodeValue<GenICam::gcstring>(pNodeMap, "DeviceSerialNumber") + ")";
+//
+//    IImage *pImage;
+//    cv::Mat img;
+//
+//    double framesPerSecond = GetNodeValue<double>(pNodeMap, "AcquisitionFrameRate");
+//
+//    for (int i = 0; i < framesPerSecond * numberOfSeconds;  i++){
+//        pImage = pDevice->GetImage(TIMEOUT);
+//        img = cv::Mat((int)pImage->GetHeight(), (int)pImage->GetWidth(), CV_8UC1, (void *)pImage->GetData());
+//        //イメージのリサイズ
+//        cv::resize(img, img, cv::Size(), 0.5, 0.5);
+//
+//        cv::imshow(windowName.c_str(), img);
+//        pDevice->RequeueBuffer(pImage);
+//        cv::waitKey(waitTime);
+//    }
+//    cv::destroyAllWindows();
+//}
 
-        cv::imshow(windowName.c_str(), img);
-        pDevice->RequeueBuffer(pImage);
-        cv::waitKey(waitTime);
-    }
-    cv::destroyAllWindows();
-}
-
-//偏光データを再生
+// 偏光データを再生
 void RunOpencvPolarizedVideo(IDevice* pDevice, GenApi::INodeMap* pNodeMap, void* pDeg0, void* pDeg45, void* pDeg90, void* pDeg135){
 
-    IImage *pImage;
+    IImage* pImage;
     cv::Mat img;
-
     double framesPerSecond = GetNodeValue<double>(pNodeMap, "AcquisitionFrameRate");
+    // Polarizedクラスを生成
+    Polarized pol_chunk(pDeg0, pDeg45, pDeg90, pDeg135);
 
     for (int i = 0; i < framesPerSecond * numberOfSeconds;  i++){
         pImage = pDevice->GetImage(TIMEOUT);
-        //偏光データを取得
-        GetPolarizeData(pImage, pDeg0, pDeg45, pDeg90, pDeg135);
-        img = cv::Mat((int)pImage->GetHeight()/2, (int)pImage->GetWidth()/2, CV_8UC1, (void *)pDeg0);
-        //イメージのリサイズ
+        // 偏光データを取得
+        pol_chunk.GetPolarizedData(pImage);
+        pol_chunk.TranslatePointerToMatrix(pImage);
+        //img = cv::Mat((int)pImage->GetHeight() / 2, (int)pImage->GetWidth() / 2, CV_8UC1, (void *)pDeg90);
+        // イメージのリサイズ
+        img = pol_chunk.deg0_mat;
+        //std::cout << img << std::endl;
         cv::resize(img, img, cv::Size(), 0.5, 0.5);
-
         cv::imshow("polarizeddata", img);
         pDevice->RequeueBuffer(pImage);
         cv::waitKey(waitTime);
@@ -133,7 +150,7 @@ void AcquireImage( IDevice* pDevice ){
 
     // RAW画像から偏光データを抽出
     std::cout << "偏光データの抽出\n";
-
+    //const unique_ptrだと管理対象オブジェクトの生存期間が作成されたスコープに限定されてしまう
     std::unique_ptr<unsigned short> deg0(new unsigned short[(WIDTH / 2 ) * ( HEIGHT / 2 )]);
     std::unique_ptr<unsigned short> deg45(new unsigned short[(WIDTH / 2 ) * ( HEIGHT / 2 )]);
     std::unique_ptr<unsigned short> deg90(new unsigned short[(WIDTH / 2 ) * ( HEIGHT / 2 )]);
@@ -146,7 +163,7 @@ void AcquireImage( IDevice* pDevice ){
     pDevice->StopStream();
 }
 
-//デバイスとの接続，IDevice* pDeviceの生成
+// デバイスとの接続，IDevice* pDeviceの生成
 int main(){
     std::cout << "カラー偏光カメラ Console Sample\n";
 
